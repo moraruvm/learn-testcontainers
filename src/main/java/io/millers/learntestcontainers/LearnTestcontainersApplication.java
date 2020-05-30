b2ec2f350fcbd6e59113f7aab03303f4fc9989f3
Vlad Moraru <Vlad.Moraru@endava.com>
2020-05-30 06:08:37 +0300

    Add project files

---
+++ src/main/java/io/millers/learntestcontainers/LearnTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package io.millers.learntestcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTestcontainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnTestcontainersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/millers/learntestcontainers/LearnTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.millers.learntestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTestcontainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
